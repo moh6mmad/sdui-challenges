afbcbba066051d9525bd235ba082529876b3e629
Mohammad <mohammad@lync.me>
2022-05-04 20:00:01 +0200

    News migration & model - Added required fillable & columns

---
+++ app/Models/News.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class News extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'title', 
+        'content', 
+        'user_id'
+    ];
+
+    /**
+     * Return news author
+     * 
+     * @return BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+}
