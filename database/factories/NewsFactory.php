1d9d7df54f5577a6f29de1d55487cd0c08bae5a7
Mohammad <mohammad@lync.me>
2022-05-05 21:21:08 +0200

    Init NewsFactory & added to database seeder

---
+++ database/factories/NewsFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\News;
+use App\Models\User;
+
+class NewsFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->realText(50),
+            'user_id' => User::all()->random()->id,
+            'content' => $this->faker->paragraph(),
+        ];
+    }
+}
