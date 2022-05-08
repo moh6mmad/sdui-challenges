dc8db4ec5f75a1b8e1f200552b5fdf4140e7ed4b
Mohammad <mohammad@lync.me>
2022-05-08 11:33:31 +0200

    Route - Added assign route to web router

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,5 @@ use Illuminate\Support\Facades\Route;
 */
 Route::group(['middleware' => ['auth']], function () {
     Route::resource('/news', NewsController::class);
+    Route::post('/news/{news}/assign', [NewsController::class, 'assign'])->name('news.assign');
 });
