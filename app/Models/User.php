bc25d4116258ff1f0592740b2e9adaa121dd2863
Mohammad <mamm6d@gmail.com>
2022-05-10 10:46:47 +0200

    Test - Added Unit test to avoid err on testing

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,9 +2,8 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -46,8 +45,8 @@ class User extends Authenticatable
     /**
      * Return the user's news
      */
-    public function news(): BelongsToMany
+    public function news(): HasMany
     {
-        return $this->belongsToMany(News::class)->withTimestamps();
+        return $this->hasMany(News::class);
     }
 }
---
+++ tests/Unit/NewsTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class NewsTest extends TestCase
+{
+    /**
+     * A basic unit test index.
+     *
+     * @return void
+     */
+    public function test_index()
+    {
+        $this->assertTrue(true);
+    }
+}
